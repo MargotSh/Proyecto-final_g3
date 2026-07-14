d72f5fbc09ddbc4b9e3189a251289f3868827fbf
MargotSh <u20261e486@upc.edu.pe>
2026-07-14 11:28:03 -0500

    Delete Taller3 directory

--- Taller3/1.2.py
+++
@@ -1,9 +0,0 @@
-def par_o_impar(numero):
-    if numero % 2 == 0:
-        print(f"{numero} es par")
-    else:
-        print(f"{numero} es impar")
-
-# Programa principal
-numero = int(input("Ingrese un número: "))
-par_o_impar(numero)
